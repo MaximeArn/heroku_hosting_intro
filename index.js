d116ca38c66e8966f5352c62da351d83c1cb0733
MaximeArn <maxime-arnould@outlook.fr>
2022-05-10 17:47:51 +0200

    add a log on get request

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+let reqCount = 0;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
+  .get("/", (req, res) => {
+    res.render("pages/index");
+    console.log(`get request was made ${reqCount} times !`);
+  })
   .listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-    setTimeout(() => console.log("MY OWN LOG"), 5000);
   });
