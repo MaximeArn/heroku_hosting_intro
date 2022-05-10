79d48ea2616d4475707a4f4345b0017a9648978b
MaximeArn <maxime-arnould@outlook.fr>
2022-05-10 19:02:18 +0200

    implement a new rout and learn how to push local changes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const coolFace = require("cool-ascii-faces");
+const PORT = 8453;
 let reqCount = 1;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => {
+  .get("/", (_, res) => {
     res.render("pages/index");
     console.log(`get request was made ${reqCount} times !`);
     reqCount++;
   })
+  .get("/cool", (_, res) => res.send(coolFace()))
   .listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
   });
