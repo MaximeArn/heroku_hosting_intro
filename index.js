f5b6e80fc9722f4f84efccb81a6dd11b73ebb1c8
MaximeArn <maxime-arnould@outlook.fr>
2022-05-10 19:37:15 +0200

    configure environment variables

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const coolFace = require("cool-ascii-faces");
 const PORT = process.env.PORT || 8453;
 let reqCount = 1;
 
+showTimes = () => {
+  let result = "";
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + " ";
+  }
+  return result;
+};
+
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
@@ -14,6 +23,7 @@ express()
     reqCount++;
   })
   .get("/cool", (_, res) => res.send(coolFace()))
+  .get("/times", (req, res) => res.send(showTimes()))
   .listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
   });
